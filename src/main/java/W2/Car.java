2394a17f3c95fbb9909013802d23faca6d5dfac9
jay <jaewhan232@naver.com>
2021-10-01 19:49:50 +0900

    feat : Car 클래스 생성자, tryMove 메소드 선언

--- src/main/java/W2/Car.java
+++ src/main/java/W2/Car.java
@@ -1,4 +1,14 @@
 package W2;
 
 public class Car {
+    private String name;
+    private int position=0;
+
+    public Car(String name){
+        this.name=name;
+    }
+
+    public void tryMove(){
+    }
+
 }
