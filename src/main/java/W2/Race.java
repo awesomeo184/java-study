a8740b9cda8039e9c979591b9d5098509ebcb2ff
jay <jaewhan232@naver.com>
2021-10-01 19:45:57 +0900

    feat : Race 클래스 생성자,setRace, setCars, doRace 메소드 선언

--- src/main/java/W2/Race.java
+++ src/main/java/W2/Race.java
@@ -1,4 +1,22 @@
 package W2;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Race {
+    ArrayList<Car> cars=new ArrayList<>();
+
+    public Race(){
+        setRace();
+    }
+
+    public void setRace(){
+    }
+
+    public void setCars(){
+
+    }
+
+    public void doRace(){
+
+    }
 }
