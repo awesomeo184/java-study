e1343b6119bbab1d48a0c4c1c5b7bf6b14992077
jay <jaewhan232@naver.com>
2021-10-02 15:23:39 +0900

    feat : Race 클래스 경주세팅메소드,경주 정보 출력 메소드 구현

--- src/main/java/W2/Race.java
+++ src/main/java/W2/Race.java
@@ -4,17 +4,48 @@ import java.util.Scanner;
 
 public class Race {
     ArrayList<Car> cars=new ArrayList<>();
+    int trial;
+
+
+    //레이스 정보 출력
+    public void printRaceInfo(){
+        System.out.printf("시도 회수 : %d",trial);
+        for (int i=0;i<cars.size();i++){
+            cars.get(i).printCarInfo();
+        }
+    }
 
     public Race(){
-        setRace();
-    }
-
-    public void setRace(){
-    }
-
-    public void setCars(){
 
     }
+
+    //경주 세팅 메소드
+    public void setRace(Scanner sc){
+        setCars(sc);
+        setTrial(sc);
+    }
+
+    //시도 회수 세팅 메소드
+    private void setTrial(Scanner sc) {
+        try{
+            System.out.println("시도 회수 입력 :");
+            this.trial=sc.nextInt();
+        }
+        catch (Exception e){
+            throw new IllegalArgumentException("잘못된 입력");
+        }
+    }
+
+    //자동차 이름 세팅 메소드
+    public void setCars(Scanner sc){
+        System.out.println("경주에 참가할 자동차 이름 입력(구분은 쉼표(,)):");
+        String[] input=(sc.nextLine()).split(",");
+        for (int i=0; i<input.length;i++){
+            cars.add(new Car(input[i]));
+        }
+    }
+
+
 
     public void doRace(){
 
