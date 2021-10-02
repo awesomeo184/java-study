54ea0246e7a6446f1732d07a1beb6f6e88847e64
jay <jaewhan232@naver.com>
2021-10-02 15:22:34 +0900

    feat : Car 클래스 자동차이름 출력 메소드 구현

--- src/main/java/W2/Car.java
+++ src/main/java/W2/Car.java
@@ -1,4 +1,6 @@
 package W2;
+import java.util.Random;
+
 
 public class Car {
     private String name;
@@ -8,7 +10,17 @@ public class Car {
         this.name=name;
     }
 
+    //전진시도
     public void tryMove(){
+        Random random=new Random();
+        if (random.nextInt(10)>=10){
+            this.position++;
+        }
     }
+    //자동차 이름 정보 출력 메소드
+    public void printCarInfo(){
+        System.out.println(this.name);
+    }
+
 
 }
