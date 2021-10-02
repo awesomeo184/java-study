d41587058609f6eaac5d3963e78eded620a7ae4f
JungTag <dbsdudxor121@naver.com>
2021-10-03 00:23:36 +0900

    feat(Car) 객체 작성

--- src/main/java/task2/racingcar/Car.java
+++ src/main/java/task2/racingcar/Car.java
@@ -1,6 +1,12 @@
 package task2.racingcar;
 
+import task2.utils.RandomUtils;
+
 public class Car {
+    private final int MIN_RANDOM_VALUE = 0;
+    private final int MAX_RANDOM_VALUE = 9;
+    private final int FORWARD_CRITERIA = 4;
+
     private final String name;
     private int position = 0;
 
@@ -8,6 +14,11 @@ public class Car {
         this.name = name;
     }
 
-    public void race() {}
+    public void race() {
+        int randomValue = RandomUtils.nextInt(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE);
+        if (randomValue >= FORWARD_CRITERIA) {
+            position += randomValue;
+        }
+    }
     // 추가 기능 구현
 }
