6d9bdeeefb04249ed82838cbe1d5c9d9fe8d5733
jay <jaewhan232@naver.com>
2021-10-02 15:24:45 +0900

    feat : App 클래스 메인 메소드 수정

--- src/main/java/W2/App.java
+++ src/main/java/W2/App.java
@@ -1,7 +1,12 @@
 package W2;
+import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args){
+        final Scanner scanner = new Scanner(System.in);
         System.out.println("Hello please");
+        Race carRace=new Race();
+        carRace.setRace(scanner);
     }
 }
